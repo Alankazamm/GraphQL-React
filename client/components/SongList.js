7e06ca748a8a995f4497383a914de4062dc41cce
Hayder Jebur <hayderjabur1983@gmail.com>
2021-02-28 23:06:30 -0800

    add song create

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,19 +1,50 @@
 import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import { gql, useQuery, useMutation } from '@apollo/client';
+import { FETCH_SONGS } from '../queries/fetchSongs';
+import { Link } from 'react-router-dom';
 
 const SongList = () => {
-  const { loading, error, data } = useQuery(SONG);
-  console.log(data);
+  const { loading, error, data } = useQuery(FETCH_SONGS);
+  // const [deleteSong] = useMutation(DELETE_SONG);
+
+  // const onSongDelete = (songId) => {
+  //   deleteSong({
+  //     variables: { id: songId },
+  //     refetchQueries: [{ query: FETCH_SONGS }],
+  //   });
+  // };
+
+  const renderSongs = () => {
+    return data.songs.map(({ id, title }) => {
+      return (
+        <li key={id} className='collection-item'>
+          {title}
+          {/* <span onClick={() => onSongDelete(id)} className={deleteSpan}>
+            X
+          </span> */}
+        </li>
+      );
+    });
+  };
+
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-  return <div>data: {data && data.songs.map((song) => song.title)}</div>;
+  if (error) return <p>error</p>;
+  return (
+    <div>
+      <ul>{renderSongs()}</ul>
+      <Link to='/songs/new' className='btn-floating btn-large red right'>
+        +
+      </Link>
+    </div>
+  );
 };
-const SONG = gql`
-  query GetSongs {
-    songs {
-      title
-    }
-  }
-`;
+
+// const DELETE_SONG = gql`
+//   mutation DeleteSong($id: ID!) {
+//     deleteSong(id: $id) {
+//       id
+//     }
+//   }
+// `;
 
 export default SongList;
--- client/index.js
+++ client/index.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
   uri: 'http://localhost:4000/graphql',
+  dataIdFromObject: (o) => o.id,
 });
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <SongList />
+      <Router>
+        <Route exact path='/' component={SongList} />
+        <Switch>
+          <Route exact path='/songs/new' component={SongCreate}></Route>
+          {/* <Route exact path="/songs/:id" component={SongDetail} /> */}
+        </Switch>
+      </Router>
     </ApolloProvider>
   );
 };
